2b3e2febc702c20dbc357bff7a5bc3ec589bc96a
claratesser <claratesserc@gmail.com>
2026-04-06 15:14:27 -0300

    feat: alteração na model user

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -7,7 +7,7 @@ from core.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'groups']
+        fields = ['id', 'email', 'name', 'telefone', 'is_active']
 
 
 class UserRegistrationSerializer(ModelSerializer):
@@ -15,7 +15,7 @@ class UserRegistrationSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'password']
+        fields = ['id', 'email', 'name', 'telefone', 'password']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
